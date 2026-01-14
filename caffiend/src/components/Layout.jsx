f5470ce71177ce37d8cd775c5d989f16239a9e04
Akash Bhat <akashkbhat216@gmail.com>
2026-01-14 22:23:30 +0530

    chore(caffiend): update footer credits

--- caffiend/src/components/Layout.jsx
+++ caffiend/src/components/Layout.jsx
@@ -31,7 +31,7 @@ export default function Layout(props) {
 
     const footer = (
         <footer>
-            <p><span className="text-gradient">Caffiend</span> was made by <a target="_blank" href="https://www.smoljames.com">Smoljames</a> <br />using the <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.<br />Check out the project on <a target="_black" href="https://www.github.com/jamezmca/reactjs-full-course">GitHub</a>!</p>
+            <p><span className="text-gradient">Caffiend</span> was made by <a target="_blank" href="https://voidomin.github.io/resume-website/">Akash</a> <br />using the <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.<br />Check out the project on <a target="_black" href="https://github.com/voidomin/react-projects.">GitHub</a>!</p>
         </footer>
     )
 
