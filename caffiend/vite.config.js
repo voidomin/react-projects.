d29918289079b540854bfc03932f4d931ba193a1
Akash Bhat <akashkbhat216@gmail.com>
2026-02-17 12:05:08 +0530

    fix: update base paths to /react-projects./ with repo name (dot)

--- caffiend/vite.config.js
+++ caffiend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/caffiend/",
+  base: "/react-projects./caffiend/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
--- todo-app/vite.config.js
+++ todo-app/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todo-app/",
+  base: "/react-projects./todo-app/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
