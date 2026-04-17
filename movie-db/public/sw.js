b9351fa28b99532ec19fd4e3104a08670ab8f94c
voidomin <voidomin@users.noreply.github.com>
2026-04-17 13:53:59 +0530

    fix: resolve blank page in Movie DB by adding router basename and fixing sw paths

--- movie-db/public/sw.js
+++ movie-db/public/sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = "movie-db-v1";
 const ASSETS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/favicon.svg"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./favicon.svg"
 ];
 
 self.addEventListener("install", (event) => {
